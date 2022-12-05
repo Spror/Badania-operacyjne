0636ce3bd7cbff1b5391698abc61c81e770d2998
Spror <wiktorspringer27@gmail.com>
2022-12-05 22:33:55 +0100

    add dijkstra to ford Fulkerson method

--- LAB_3/main.cpp
+++ LAB_3/main.cpp
@@ -3,9 +3,12 @@
 #include <fstream>
 #include <chrono>
 #include <queue>
+#include <set>
+#include <list>
+
+
+
 using namespace std;
-
-
 using matrix = vector<vector<int>>;
 
 /* czytanie danych z pliku */
@@ -62,11 +65,10 @@ void print_graph(matrix graph)
 }
 
 
-bool findPath(matrix &graph, int start, int final, vector<int> &path)
+bool findPath(matrix graph, int start, int final, vector<int> &path)
 {
     vector<bool> visited;
     visited.resize(graph.size(), 0);
-
 
     queue<int> qset;
     qset.push(start);
@@ -78,7 +80,7 @@ bool findPath(matrix &graph, int start, int final, vector<int> &path)
         auto u = qset.front();
         qset.pop();
 
-        for(auto i = 0; i < graph.size(); i++)
+        for(int i = 0; i < graph.size(); i++)
         {
             if(visited[i] == false && graph[u][i] > 0)
             {
@@ -91,12 +93,60 @@ bool findPath(matrix &graph, int start, int final, vector<int> &path)
                 path[i] = u;
                 visited[i] = true;
             }
-
         }
     }
     return false;
-
 }
+
+bool dijkstraAlgorithm(matrix graph, int start, int final, vector<int> &path )
+{
+    auto graph_size = graph.size();
+    vector<int> minimal_distance, previous;
+    minimal_distance.clear();
+    minimal_distance.resize(graph_size, INT32_MAX);
+    minimal_distance[start] = 0;
+    previous.resize(graph_size, -1);
+    
+    set<pair<int,int>> ver_queue;
+    ver_queue.insert(make_pair(minimal_distance[start],start));
+
+    while(!ver_queue.empty())
+    {
+        auto dist = ver_queue.begin()->first;
+        auto u = ver_queue.begin()->second;
+        ver_queue.erase(ver_queue.begin());
+        
+        const auto neighbors = graph[u];
+        for(auto i = 0; i < graph_size; i++)
+        {
+            if(graph[u][i] != 0 )
+            {
+                auto v = i;
+                auto weight = graph[u][i];
+                auto distance_through_u = dist + weight;
+                if (distance_through_u < minimal_distance[v]) 
+                {
+                    ver_queue.erase(make_pair(minimal_distance[v], v));
+                    minimal_distance[v] = distance_through_u;
+                    previous[v] = u;
+                    ver_queue.insert(make_pair(minimal_distance[v], v));
+
+                }
+            }
+        }
+    }
+    path.clear();
+    path.resize(graph_size, 0);
+    auto inc =final;
+    for ( ; inc != -1; inc = previous[inc])
+        path[inc] = previous[inc];
+
+    if(path[final] == -1)
+        return false;
+
+    return true;
+}
+
 
 void fordFulkerson(matrix graph, int start, int final)
 {
@@ -104,13 +154,15 @@ void fordFulkerson(matrix graph, int start, int final)
     auto graph_size = graph.size();
     auto maxFlow = 0;
     vector<int> path;
-    path.resize(graph_size);
+    path.resize(graph_size, 0);
     
-
+  
     
-    while(findPath(graph_copy, start, final, path))
+    while(/*findPath(graph_copy, start, final, path)*/ dijkstraAlgorithm(graph_copy, start, final, path))
     {
-        auto pathFlow = INT32_MAX;
+
+        cout << endl;
+        int pathFlow = INT32_MAX;
 
         for(auto i = final; i!=start; i = path[i])
         {
@@ -128,9 +180,7 @@ void fordFulkerson(matrix graph, int start, int final)
         maxFlow += pathFlow;
     }
 
-
     cout << "max flow: " << maxFlow << endl;
-
 }
 
 int main()
@@ -140,9 +190,8 @@ int main()
     if(!reading_from_file("dane2.txt", graph)){ cout << "!poszlo" << endl; }
     print_graph(graph);
 
-    fordFulkerson(graph,0 ,7);
- 
-
-
+    fordFulkerson(graph, 0, graph.size()-1);
+   
+    
     return 0;
 }
